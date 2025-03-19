5f0c8e89d02dd2cd13f57cec009621bc74f78438
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2025-03-19 11:09:11 +0100

    Add asynchronous I/O research

--- ShipAndThread/obj/Debug/net9.0/ShipAndThread.AssemblyInfo.cs
+++ ShipAndThread/obj/Debug/net9.0/ShipAndThread.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShipAndThread")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c17f1d76a5304c06bc3c7e14ded47e4e2d5a2537")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38ddaf4da7d6acfe1747ad45ab4f98756814c76b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShipAndThread")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShipAndThread")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
