bede9e73888b2126affb247e47649bbf898550f9
guntzel-sebastian <sebastian.guntzel@student.nhlstenden.com>
2025-04-01 21:29:31 +0200

    Removed build and generated files

--- ShipAndThread/Application/DTOs/TruckUpdateDto.cs
+++
@@ -1,8 +0,0 @@
-namespace ShipAndThread.Application.DTOs;
-
-public class TruckUpdateDto
-{
-    public string LicensePlate { get; set; }
-    public double X { get; set; }
-    public double Y { get; set; }
-}
