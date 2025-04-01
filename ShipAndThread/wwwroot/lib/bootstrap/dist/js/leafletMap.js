bb5d31a54e455b3e4c0fc64018c2feb3a70440be
CaterinaAresti <111961519+CaterinaAresti@users.noreply.github.com>
2025-04-01 20:13:11 +0200

    Fixed cargo cards and search bar styling, tried fixing map

---
+++ ShipAndThread/wwwroot/lib/bootstrap/dist/js/leafletMap.js
@@ -0,0 +1,21 @@
+﻿window.initMap = (mapId, latitude, longitude, zoom) => {
+    console.log("Initializing Leaflet Map:", mapId, latitude, longitude, zoom);
+    
+    var map = L.map(mapId).setView([latitude, longitude], zoom);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
+
+    L.marker([latitude, longitude]).addTo(map)
+        .bindPopup('Selected Location')
+        .openPopup();
+
+    setTimeout(() => {
+        map.invalidateSize();
+    }, 500);
+
+    window.addEventListener("resize", () => {
+        setTimeout(() => map.invalidateSize(), 200);
+    });
+};
