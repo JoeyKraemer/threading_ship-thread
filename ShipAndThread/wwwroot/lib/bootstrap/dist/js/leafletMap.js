6688abd84910cd19097f0472530eda4ef1da66a0
CaterinaAresti <111961519+CaterinaAresti@users.noreply.github.com>
2025-04-08 15:54:53 +0200

    Added Activity Log + markers for trucks in Home page

--- ShipAndThread/wwwroot/lib/bootstrap/dist/js/leafletMap.js
+++ ShipAndThread/wwwroot/lib/bootstrap/dist/js/leafletMap.js
@@ -1,15 +1,14 @@
-﻿window.initMap = (mapId, latitude, longitude, zoom) => {
+﻿window.maps = {};
+
+window.initMap = (mapId, latitude, longitude, zoom) => {
     console.log("Initializing Leaflet Map:", mapId, latitude, longitude, zoom);
     
     var map = L.map(mapId).setView([latitude, longitude], zoom);
+    window.maps[mapId] = { map: map, markers: [] };
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; OpenStreetMap contributors'
     }).addTo(map);
-
-    L.marker([latitude, longitude]).addTo(map)
-        .bindPopup('Selected Location')
-        .openPopup();
 
     setTimeout(() => {
         map.invalidateSize();
@@ -18,4 +17,26 @@
     window.addEventListener("resize", () => {
         setTimeout(() => map.invalidateSize(), 200);
     });
-};
+
+    window.addTruckMarkers = (mapId, truckList) => {
+        const mapObj = window.maps[mapId];
+        if (!mapObj) return;
+
+        mapObj.markers.forEach(marker => mapObj.map.removeLayer(marker));
+        mapObj.markers = [];
+
+        truckList.forEach(truck => {
+                const marker = L.circleMarker([truck.latitude, truck.longitude], {
+                radius: 9,
+                fillColor: truck.color || "gray",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            }).addTo(mapObj.map)
+                .bindPopup(`Truck ${truck.truckId}`);
+
+            mapObj.markers.push(marker);
+        });
+    }
+}
